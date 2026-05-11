7a35b630f6e5223ef32259094d7a00ab7a482834
eckennedy-cloud <eckennedy@stu.naperville203.org>
2026-05-11 08:33:59 -0500

    new

--- ColorLabs17/collage.java
+++ ColorLabs17/collage.java
@@ -11,12 +11,22 @@ public class collage
         Picture ballerina = new Picture("images\\ballerina1.jpg");
         Picture canvas = new Picture("images\\canvas.jpg");
         copytoCanvas(ballerina,canvas);
+        
         negate(ballerina);
         copytoCanvas(ballerina,canvas);
+        
         Picture ballerina2 = new Picture("images\\ballerina1.jpg");
         mirrorVertical(ballerina2);
         copytoCanvas(ballerina2,canvas);
         canvas.explore();
+        
+        Picture ballerina3 = new Picture("images\\ballerina1.jpg");
+        Sepia(ballerina3);
+        copytoCanvas(ballerina3,canvas);
+        canvas.explore();
+        
+        Picture ballerina4 = new Picture("images\\ballerina1.jpg");
+        recursion(ballerina4,canvas);
     }
     public static void copytoCanvas(Picture source, Picture target){
         Pixel sourcePix=null;
@@ -87,12 +97,15 @@ public class collage
         }
         }
         else{
-        for (int sourceX=0,targetX=10;sourceX<source.getWidth()&&targetX<canvas.getWidth();sourceX++,targetX++){
-            for (int sourceY=0,targetY=10; sourceY<source.getHeight()&&targetY<canvas.getHeight();sourceY++,targetY++){
+         
+        int[][] Pixels = new int[source.getWidth()/2][source.getHeight()/2];
+        for (int sourceX=0,targetX=10;sourceX<source.getWidth()&&targetX<canvas.getWidth();sourceX+=2,targetX++){
+            for (int sourceY=0,targetY=10; sourceY<source.getHeight()&&targetY<canvas.getHeight();sourceY+=2,targetY++){
                 Pixel sourcePix= source.getPixel(sourceX,sourceY);
                 Pixel targetPix=canvas.getPixel(targetX,targetY);
                 if (targetPix.getRed()==255 && targetPix.getGreen()==255 && targetPix.getBlue()==255){
                     targetPix.setColor(sourcePix.getColor());
+                    Pixels[sourceX][sourceY]=targetPix;
                 }
                 else sourceY--;
                 if (targetY>=6490){
@@ -101,17 +114,26 @@ public class collage
                 }
             }
         }
-            int newH=source.getHeight()/2;
-            int newW=source.getWidth()/2;
-            int Yratio = source.getHeight()/newH;
-            int Xratio = source.getWidth()/newW;
-            int[] newPixels = new int[newH*newW];
-            for (int y=0; y<newH;y++){
-                for (int x=0; x<newW;x++){
-                    
-                }
-            }
+        recursion( source,canvas);
         }
         
-}
-}
+    }
+    public static void Sepia(Picture source){
+        //negate:
+        Pixel[] pixels2;
+        pixels2 = source.getPixels();
+        
+        for (Pixel spot1 : pixels2)
+        {
+            int red = spot1.getRed();
+            int blue = spot1.getBlue();
+            int green = spot1.getGreen();
+            int r= (int)((red * 0.393) + (green * 0.769) + (blue * 0.189));
+            spot1.setRed(r);
+            int b = (int)((red * 0.272) + (green * 0.534) + (blue * 0.131));
+            spot1.setBlue(b);
+            int g = (int)((red * 0.349) + (green * 0.686) + (blue * 0.168));
+            spot1.setGreen(g);
+        }
+    }
+    }
