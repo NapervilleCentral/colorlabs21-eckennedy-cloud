7b560ead61b306edb0a99f098fced24012f3f65d
eckennedy-cloud <eckennedy@stu.naperville203.org>
2026-05-14 08:29:39 -0500

    new

--- ColorLabs17/collage.java
+++ ColorLabs17/collage.java
@@ -18,15 +18,19 @@ public class collage
         Picture ballerina2 = new Picture("images\\ballerina1.jpg");
         mirrorVertical(ballerina2);
         copytoCanvas(ballerina2,canvas);
-        canvas.explore();
         
         Picture ballerina3 = new Picture("images\\ballerina1.jpg");
         Sepia(ballerina3);
         copytoCanvas(ballerina3,canvas);
-        canvas.explore();
         
         Picture ballerina4 = new Picture("images\\ballerina1.jpg");
-        recursion(ballerina4,canvas);
+        recursion(ballerina4,ballerina4.getWidth(),ballerina4.getHeight());
+        copytoCanvas(ballerina4,canvas);
+        
+        Picture ballerina5 = new Picture("images\\ballerina1.jpg");
+        scale(ballerina5);
+        copytoCanvas(ballerina5,canvas);
+        canvas.explore();
     }
     public static void copytoCanvas(Picture source, Picture target){
         Pixel sourcePix=null;
@@ -79,42 +83,27 @@ public class collage
             }
         }
     }
-    public static void recursion(Picture source,Picture canvas){
-        if (source.getWidth()<=10){
-        for (int sourceX=0,targetX=10;sourceX<source.getWidth()&&targetX<canvas.getWidth();sourceX++,targetX++){
-            for (int sourceY=0,targetY=10; sourceY<source.getHeight()&&targetY<canvas.getHeight();sourceY++,targetY++){
-                Pixel sourcePix= source.getPixel(sourceX,sourceY);
-                Pixel targetPix=canvas.getPixel(targetX,targetY);
-                if (targetPix.getRed()==255 && targetPix.getGreen()==255 && targetPix.getBlue()==255){
-                    targetPix.setColor(sourcePix.getColor());
-                }
-                else sourceY--;
-                if (targetY>=6490){
-                    targetY=0;
-                    targetX+=2126;
+    public static void recursion(Picture source, int pictureLength, int pictureHeight){
+        if (pictureLength<=50){
+            for (int x=0, sx=1000; x<pictureLength-1 && sx<pictureLength;x+=2, sx++){
+                for (int y=0, sy=1000; y<pictureHeight && sy<pictureHeight;y+=2,sy++){
+                    Color color=source.getPixel(x,y).getColor();
+                    source.getPixel(sx,sy).setColor(color);
                 }
             }
-        }
         }
         else{
-         
-        int[][] Pixels = new int[source.getWidth()/2][source.getHeight()/2];
-        for (int sourceX=0,targetX=10;sourceX<source.getWidth()&&targetX<canvas.getWidth();sourceX+=2,targetX++){
-            for (int sourceY=0,targetY=10; sourceY<source.getHeight()&&targetY<canvas.getHeight();sourceY+=2,targetY++){
-                Pixel sourcePix= source.getPixel(sourceX,sourceY);
-                Pixel targetPix=canvas.getPixel(targetX,targetY);
-                if (targetPix.getRed()==255 && targetPix.getGreen()==255 && targetPix.getBlue()==255){
-                    targetPix.setColor(sourcePix.getColor());
-                    Pixels[sourceX][sourceY]=targetPix;
-                }
-                else sourceY--;
-                if (targetY>=6490){
-                    targetY=0;
-                    targetX+=2126;
+            for (int x=0, sx=1000; x<pictureLength;x+=2,sx++){
+                for (int y=0, sy=1300; y<pictureHeight;y+=2,sy++){
+                    Color color=source.getPixel(x,y).getColor();
+                    if (!(color.getRed()<50 && color.getGreen()<50 && color.getBlue()<50))
+                        source.getPixel(sx,sy).setColor(color);
+                    
                 }
             }
-        }
-        recursion( source,canvas);
+            pictureLength=pictureLength/2;
+            pictureHeight=pictureHeight/2;
+            recursion(source, pictureLength,pictureHeight);
         }
         
     }
@@ -136,4 +125,60 @@ public class collage
             spot1.setGreen(g);
         }
     }
+    public static void Rotate(Picture source){
+        Pixel leftPixel=null;
+        Pixel rightPixel=null;
+        for (int x=0; x<source.getWidth()-1;x++){
+            for (int y=0; y<source.getWidth();y++){
+                leftPixel=source.getPixel(x,y);
+                rightPixel=source.getPixel(y,x);
+                Color original = rightPixel.getColor();
+                rightPixel.setColor(leftPixel.getColor());
+                leftPixel.setColor(original);
+            }
+        }
     }
+    public static void scale(Picture source){
+        
+        Pixel [] pixels1;
+        int length =0;
+        pixels1=source.getPixels();
+        for(Pixel spot1: pixels1){
+    
+        int red=spot1.getRed();
+        int green=spot1.getGreen();
+        int blue=spot1.getGreen();
+        int gray= (int)((blue+green+red)/3);
+        if (gray>=0 && gray<70){
+            spot1.setRed(128);
+            spot1.setGreen(0);
+            spot1.setBlue(128);
+        }
+        if (gray>=70 && gray<100){
+            spot1.setRed(255);
+            spot1.setGreen(102);
+            spot1.setBlue(204);
+        }
+        if (gray>=100 && gray<130){
+            spot1.setRed(230);
+            spot1.setGreen(180);
+            spot1.setBlue(200);
+        }
+        if (gray>=130 && gray<200){
+            spot1.setRed(255);
+            spot1.setGreen(200);
+            spot1.setBlue(200);
+        }
+        if (gray>=200&&gray<230){
+            spot1.setRed(90);
+            spot1.setGreen(70);
+            spot1.setBlue(120);
+        }
+        if (gray>=230 && gray<255){
+            spot1.setRed(150);
+            spot1.setGreen(42);
+            spot1.setBlue(85);
+        }
+        }
+    }
+}
